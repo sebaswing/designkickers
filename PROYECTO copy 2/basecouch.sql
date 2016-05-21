efa2d38bd3767d29a17f1d7dc2d4ae1b81f3e019
maximilianofni <maximilianofni@gmail.com>
2016-05-21 19:42:33 -0300

    bd
    base de datos

---
+++ PROYECTO copy 2/basecouch.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2016 a las 00:41:42
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.5.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `basecouch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `mail` varchar(100) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `admin`
+--
+
+INSERT INTO `admin` (`mail`, `password`) VALUES
+('angelica@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE IF NOT EXISTS `comentarios` (
+  `id_comentarios` int(11) NOT NULL,
+  `id_couch` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `pregunta` varchar(80) NOT NULL,
+  `respuesta` varchar(250) NOT NULL,
+  `mail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotografia`
+--
+
+CREATE TABLE IF NOT EXISTS `fotografia` (
+  `id_couch` int(11) NOT NULL,
+  `id_fotografia` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `link` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE IF NOT EXISTS `pagos` (
+  `idpagos` int(11) NOT NULL,
+  `monto` int(11) NOT NULL,
+  `fechaPago` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE IF NOT EXISTS `reserva` (
+  `idreserva` int(11) NOT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  `idcouch` varchar(45) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_fin` datetime NOT NULL,
+  `comment_couch` varchar(250) DEFAULT NULL,
+  `puntaje_couch` varchar(250) DEFAULT NULL,
+  `comment_huesped` varchar(250) DEFAULT NULL,
+  `puntaje_huesped` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `mail` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `fecha_nac` datetime NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `telefono` int(10) unsigned NOT NULL,
+  `fechavencimiento` datetime DEFAULT NULL,
+  `numTarjeta` int(10) unsigned DEFAULT NULL,
+  `id_pagos` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`mail`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`),
+  ADD UNIQUE KEY `idcategoria_UNIQUE` (`id_categoria`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentarios`);
+
+--
+-- Indices de la tabla `fotografia`
+--
+ALTER TABLE `fotografia`
+  ADD PRIMARY KEY (`id_fotografia`),
+  ADD UNIQUE KEY `link_UNIQUE` (`link`);
+
+--
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`idpagos`),
+  ADD UNIQUE KEY `idpagos_UNIQUE` (`idpagos`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`idreserva`),
+  ADD UNIQUE KEY `idreserva_UNIQUE` (`idreserva`),
+  ADD UNIQUE KEY `idcouch_UNIQUE` (`idcouch`),
+  ADD UNIQUE KEY `mail_UNIQUE` (`mail`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`mail`),
+  ADD UNIQUE KEY `id_pagos_UNIQUE` (`id_pagos`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentarios` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `fotografia`
+--
+ALTER TABLE `fotografia`
+  MODIFY `id_fotografia` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `idpagos` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `idreserva` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
