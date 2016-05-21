e5822a7cb8a9c6c7bc6ffca22e3aef6e7ca93a2d
maximilianofni <maximilianofni@gmail.com>
2016-05-21 19:21:38 -0300

    correccion de los estilos
    correccion de los estilos y el archivo javascript

---
+++ PROYECTO copy 2/conexion.php
@@ -0,0 +1,10 @@
+<?php  // funcion php para la conexion con la base de datos
+function conectar(){
+	$link = mysqli_connect('localhost', 'root', '', 'grupo14')// primero localhost, root,sinclave,nomdela		
+	or die("Error " . mysqli_error($link));
+	mysqli_query($link,"SET NAMES 'utf8'");// esta funcion te coloca los asentos automaticamente en la bd
+	mysqli_query($link,"SET CHARACTER SET utf8");
+	mysqli_query($link,"SET COLLATION_CONNECTION = 'utf8_unicode_ci'"); 
+	return $link;
+}
+?>
