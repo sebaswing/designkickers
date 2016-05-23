bbb920cb5828383cc09eee1b3ee2a2a3d037f510
maximilianofni <maximilianofni@gmail.com>
2016-05-23 17:22:21 -0300

    actualizacion de procesar usuario
    subiendo la ultima actualizacion de procesar usuario

--- PROYECTO copy 2/procesarusuario.php
+++ PROYECTO copy 2/procesarusuario.php
@@ -7,14 +7,26 @@
    $nom=$_POST['nom'];
    $tel=$_POST['numeroTel'];
    $fecha=$_POST['fechanac'];
+   $consulta = "select * from usuario where mail = '$cor'";
+   $usuario = mysqli_query($link , $consulta);
+   $row = mysqli_num_rows($usuario);
+   if ($row == 1 ) {
+      ?>
+      <script>
+        alert("existe el mail con ese mismo nombre de usuario");
+        window.location.href="usuariocomun.php";
+       </script> 
+       <?php
+   }
    $sql= "INSERT INTO usuario(mail,password,fecha_nac,apellido,nombre,telefono)
       values('$cor','$cla','$fecha','$ape','$nom',$tel)";
-     $insertarusu1 = mysqli_query($link , $sql);
-      echo $sql;
+     $insertarusu1 = mysqli_query($link, $sql);
+    // if (!$insertarusu1) { die ("error en consulta: ".mysql_error());
+    
      ?>
-      <script>
+     <!-- <script>
         alert("se inserto correctamente");
         window.location.href="usuariocomun.php";
-       </script>  
+       </script> -->  
      <?php
 ?>
